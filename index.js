36f866c85d32c78418e1bdaecd2c592fcbbaa9b1
ayushxx01 <ayushxx01s@gmail.com>
2026-06-02 22:43:23 +0530

    bot completed

--- index.js
+++ index.js
@@ -1,17 +1,63 @@
 require('dotenv').config();
-const { Client, GatewayIntentBits } = require('discord.js');
+const { Client, GatewayIntentBits,   ActionRowBuilder,
+  ButtonBuilder,
+  ButtonStyle } = require('discord.js');
 
 
 const client = new Client({
     intents: [GatewayIntentBits.Guilds],
 });
 
-client.once("ready", async () => {
-  console.log(`✅ Logged in as ${client.user.tag}`);
+client.once("clientReady", async () => {
 
-   const channel = await client.channels.fetch('1511349091463069768');
+    try {
+        const row = new ActionRowBuilder()
+        .addComponents(
+        new ButtonBuilder().
+        setCustomId('approve').
+        setLabel("Approve").
+        setStyle(ButtonStyle.Success),
 
-   await channel.send("Devlog online, Hare krishna");
+        new ButtonBuilder().
+        setCustomId('skip').
+        setLabel("Skip").
+        setStyle(ButtonStyle.Danger)
+    )
+          console.log(`✅ Logged in as ${client.user.tag}`);
+
+          const channel = await client.channels.fetch(process.env.CHANNEL_ID);
+
+          await channel.send({
+            content: "Devlog online, Hare krishna",
+            components: [row]
+          });
+    } catch (error) {
+        console.error("Error sending message:", error);
+    }
+
+
 });
+client.on('interactionCreate', async(interaction)=> {
+        if(!interaction.isButton()) return;
+
+            console.log(
+        interaction.customId,
+        interaction.message.id
+    );
+        console.log(`Button Clicked: ${interaction.customId}`);
+
+     if(interaction.customId === 'approve') {
+        await interaction.update({
+    content: `Post approved: ${interaction.customId}`,
+    components: []
+}  
+    );
+    }
+else {
+    await interaction.update({
+        content: `Post skipped: ${interaction.customId}`,
+        components: []
+    });
+}});
 
 client.login(process.env.DISCORD_TOKEN);
