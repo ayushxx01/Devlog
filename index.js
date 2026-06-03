a84cd123d286b5db0f1c686dc734ec320c7ce8bb
ayushxx01 <ayushxx01s@gmail.com>
2026-06-04 00:11:01 +0530

    test-2

--- index.js
+++ index.js
@@ -84,7 +84,6 @@ app.post('/githook', async (req,res)=> {
     console.error("Error processing webhook:", error);
     res.status(500).send("Internal Server Error");
  }
-
 });
 
 app.post('/', (req,res)=> {
