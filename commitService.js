4a8380073d281e83ad38a9f0b1663eb8c55a7f1e
ayushxx01 <ayushxx01s@gmail.com>
2026-06-04 00:17:54 +0530

    minor fix

--- commitService.js
+++ commitService.js
@@ -1,8 +1,8 @@
-const {Pool} = require('pg');
+const Pool = require('./db');
 
 async function saveCommit(repo, commit_hash, message, commitTime){
     await Pool.query(
-        'INSERT INTO commits (repoo, commit_hash, message, commit_time) VALUES ($1, $2, $3, $4)',
+        'INSERT INTO commits (repo, commit_hash, message, commit_time) VALUES ($1, $2, $3, $4)',
         [repo, commit_hash, message, commitTime]
     );
 }
