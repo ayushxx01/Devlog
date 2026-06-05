839641ad7dfbb544ca40e4d91e62179f5b31269a
ayushxx01 <ayushxx01s@gmail.com>
2026-06-05 21:46:31 +0530

    testing for time based flow

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ client.once("clientReady", async () => {
     console.log(`✅ Logged in as ${client.user.tag}`);
 });
 
-cron.schedule('58 17 * * *', async () => {
+cron.schedule('50 21 * * *', async () => {
     console.log("Time to fetch today's summaries");
     const res = await getTodayCommits();
     const summary = await generateSummary(res);
