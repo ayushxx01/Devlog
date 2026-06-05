caddca4d2a91c7b4c33d6524ba0c2fc67b5be924
ayushxx01 <ayushxx01s@gmail.com>
2026-06-05 21:53:53 +0530

    retest via cron

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ client.once("clientReady", async () => {
     console.log(`✅ Logged in as ${client.user.tag}`);
 });
 
-cron.schedule('50 21 * * *', async () => {
+cron.schedule('* * * * *', async () => {
     console.log("Time to fetch today's summaries");
     const res = await getTodayCommits();
     const summary = await generateSummary(res);
