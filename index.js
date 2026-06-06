4a3cc08b3ea60fefb46a087a51f74269592e6632
ayushxx01 <ayushxx01s@gmail.com>
2026-06-06 13:24:12 +0530

    first test run for a fixed time frame

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ client.once("clientReady", async () => {
 });
 
 
-cron.schedule('* * * * *', async () => {
+cron.schedule('30 8 * * *', async () => {
     console.log("Time to fetch today's summaries");
     console.log("Fetching today's commits from the database...");
     const res = await getTodayCommits();
